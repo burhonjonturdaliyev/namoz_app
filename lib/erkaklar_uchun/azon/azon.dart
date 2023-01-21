2d890b2c56df864e0888b71adcd6a9d473371ad0
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-01-21 16:02:16 +0500

    azon add

--- lib/erkaklar_uchun/azon/azon.dart
+++ lib/erkaklar_uchun/azon/azon.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class Azon extends StatefulWidget {
   const Azon({super.key});
@@ -9,13 +9,76 @@ class Azon extends StatefulWidget {
 }
 
 class _AzonState extends State<Azon> {
+  final player = AudioPlayer();
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  String formatTime(int seconds) {
+    return '${(Duration(seconds: seconds))}'.split('.')[0].padLeft(8, '0');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.playing;
+      });
+    });
+
+    player.onDurationChanged.listen((newDuration) {
+      setState(() {
+        duration = newDuration;
+      });
+    });
+
+    player.onPositionChanged.listen((newPosition) {
+      setState(() {
+        position = newPosition;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ElevatedButton(
-          child: const Text("Click this"),
-          onPressed: () {},
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  child: IconButton(
+                    icon: Icon(
+                      isPlaying ? Icons.pause : Icons.play_arrow,
+                    ),
+                    onPressed: () {
+                      if (isPlaying) {
+                        player.pause();
+                      } else {
+                        player.play(AssetSource('azon.mp3'));
+                      }
+                    },
+                  ),
+                ),
+                Slider(
+                  min: 0,
+                  max: duration.inSeconds.toDouble(),
+                  value: position.inSeconds.toDouble(),
+                  onChanged: (value) {
+                    final position = Duration(seconds: value.toInt());
+                    player.seek(position);
+                    player.resume();
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
