12ede3889f58d38ae78a51281e8c986b224b1ad5
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-01-22 16:16:11 +0500

    animation

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:namoz_app/home_screen.dart';
 
@@ -19,7 +20,7 @@ class _Splash_screenState extends State<Splash_screen> {
   void initState() {
     super.initState();
     Timer(
-      const Duration(seconds: 3),
+      const Duration(seconds: 4),
       () {
         Navigator.of(context).pushAndRemoveUntil(
             MaterialPageRoute(builder: (context) => const Home_screen()),
@@ -31,21 +32,49 @@ class _Splash_screenState extends State<Splash_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blue,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "Images/splash.png",
-              width: MediaQuery.of(context).size.width * .95,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-            )
-          ],
-        ));
+      backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "Images/splash.png",
+                width: MediaQuery.of(context).size.width * .95,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .1,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .1,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              AnimatedTextKit(animatedTexts: [
+                TyperAnimatedText("Biz bilan qoling!",
+                    textStyle: TextStyle(color: Colors.white)),
+                TyperAnimatedText("Biz bilan Namoz o'qishni o'rganing!",
+                    textStyle: TextStyle(color: Colors.white))
+              ])
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
