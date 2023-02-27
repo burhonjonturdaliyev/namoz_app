bf458fdcef2db2e9700475b54125b024cdec2e40
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-02-27 14:04:01 +0500

    New updates

--- lib/splash_screen/splash_screen.dart
+++ lib/splash_screen/splash_screen.dart
@@ -31,9 +31,11 @@ class _Splash_screenState extends State<Splash_screen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Column(
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      builder: (context, child) => Scaffold(
+          backgroundColor: Colors.blue,
+          body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
